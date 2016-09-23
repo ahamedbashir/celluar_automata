a7546c2fa58a3211ddfeb957ad63f666e3c31468
NFN Jotei <joteipreet@gmail.com>
2016-09-23 13:18:19 +0000

    gol.cpp edited online with Bitbucket

--- gol.cpp
+++ gol.cpp
@@ -1,3 +1,17 @@
+/*
+ * CSc103 Project 3: Game of Life
+ * See readme.html for details.
+ * Please list all references you made use of in order to complete the
+ * assignment: your classmates, websites, etc.  Aside from the lecture notes
+ * and the book, please list everything.  And remember- citing a source does
+ * NOT mean it is okay to COPY THAT SOURCE.  What you submit here **MUST BE
+ * YOUR OWN WORK**.
+ * References:
+ *
+ *
+ * Finally, please indicate approximately how many hours you spent on this:
+ * #hours: 
+
 */
 
 #include <cstdio>
@@ -66,7 +80,7 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-void mainLoop() {
-
+void mainloop() {
+		/* update, write, sleep */
 
 }

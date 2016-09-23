75d4d4145f042cab0965d6e54c6f2dde10fc8d24
Bashir Ahamed <bahamed000@citymail.cuny.edu>
2016-09-23 18:59:34 -0400

    Function neighbor count defined...

--- gol.cpp
+++ gol.cpp
@@ -11,31 +11,29 @@
  *
  * Finally, please indicate approximately how many hours you spent on this:
  * #hours: 
-
-*/
+ */
 
 #include <cstdio>
-#include <stdlib.h>
-#include <getopt.h>
-#include <unistd.h>
+#include <stdlib.h> // for exit();
+#include <getopt.h> // to parse long arguments.
+#include <unistd.h> // sleep
 #include <vector>
-#include <string>
-
 using std::vector;
+#include <string>
 using std::string;
 
-static const char* usage = 
+static const char* usage =
 "Usage: %s [OPTIONS]...\n"
-"Text-Based version of Conway's Game of Life.\n\n"
-"   --seed,-s	 FILE	read start state from FILE.\n"
-"   --world,-w   FILE	store vurrent world in FILE.\n"
-"   --fast-fw,-f NUM	evolve system for NUM generations and quit.\n"
-"   --help,-h		show this message and exit.\n";
+"Text-based version of Conway's game of life.\n\n"
+"   --seed,-s     FILE     read start state from FILE.\n"
+"   --world,-w    FILE     store current world in FILE.\n"
+"   --fast-fw,-f  NUM      evolve system for NUM generations and quit.\n"
+"   --help,-h              show this message and exit.\n";
 
-size_t max_gen = 0; // if > 0, fast forward to this generation
-string wfilename = "/tmp/gol-world-current"; // write state here
-FILE* fworld = 0; // wfilename handler
-string initfilename = wfilename; //read initial stat from here
+size_t max_gen = 0; /* if > 0, fast forward to this generation. */
+string wfilename =  "/tmp/gol-world-current"; /* write state here */
+FILE* fworld = 0; /* handle to file wfilename. */
+string initfilename = "/tmp/gol-world-current"; /* read initial state from here. */
 
 size_t nbrCount(size_t i, size_t j, const vector<vector<bool> >& g);
 void update();
@@ -43,21 +41,22 @@ int initFromFile(const string& fname);
 void mainLoop();
 void dumpState(FILE* f);
 
-char text[3] = ".0";
+char text[3] = ".O";
 
 int main(int argc, char *argv[]) {
+	// define long options
 	static struct option long_opts[] = {
-		{"seed", required_argument, 0, 's'},
-		{"world", required_argument, 0, 'w'},
-		{"fast-fw", required_argument, 0, 'f'};
-		{"help", no_argument, 0, 'h'},
+		{"seed",    required_argument, 0, 's'},
+		{"world",   required_argument, 0, 'w'},
+		{"fast-fw", required_argument, 0, 'f'},
+		{"help",    no_argument,       0, 'h'},
 		{0,0,0,0}
 	};
+	// process options:
 	char c;
 	int opt_index = 0;
-	while((c = getopt_long(argc, argv, "hs:w:f:", long_opts, &opt_index))
-	{
-		switch(c) {
+	while ((c = getopt_long(argc, argv, "hs:w:f:", long_opts, &opt_index)) != -1) {
+		switch (c) {
 			case 'h':
 				printf(usage,argv[0]);
 				return 0;
@@ -70,17 +69,45 @@ int main(int argc, char *argv[]) {
 			case 'f':
 				max_gen = atoi(optarg);
 				break;
-			case'?':
-				printf(usage, argv[0]);
+			case '?':
+				printf(usage,argv[0]);
 				return 1;
 		}
 	}
-	
+
 	mainLoop();
 	return 0;
 }
 
-void mainloop() {
-		/* update, write, sleep */
+void mainLoop() {
+	/* update, write, sleep */
+}
 
+size_t nbrCount(size_t i, size_t j, const vector<vector<bool> >& g{
+
+		size_t row_size = g.size();
+		size_t col_size = g[i].size();
+		for ( i = 0; i < row_size; ++i)
+			for ( j = 0; j < col_size; ++j){
+				size_t count = 0;
+				if ( g[(row_size-1 + i)% row_size][j] == true )
+					count++;
+				if ( g[(row_size-1 + i)% row_size][j-1] == true )
+					count++;
+				if ( g[(row_size-1 + i)% row_size][j+1] == true )
+					count++;
+				if ( g[(row_size+1 + i)% row_size][j] == true )
+					count++;
+				if ( g[(row_size+1 + i)% row_size][j-1] == true )
+					count++;
+				if ( g[(row_size+1 + i)% row_size][j+1] == true )
+					count++;
+				if ( g[i][(col_size -1 +j)%col_size] == true )
+					count++;
+				if ( g[i][(col_size +1 +j)%col_size] == true )
+					count++;
+
+				return count;
+
+		}
 }

6c99cbd0e96ec26ce0c54ce3e3c4777be528bb38
Bashir Ahamed <bahamed000@citymail.cuny.edu>
2016-09-25 11:15:45 -0400

    initfromfile still not working

--- gol.cpp
+++ gol.cpp
@@ -43,30 +43,6 @@ string initfilename = "/tmp/gol-world-current"; /* read initial state from here.
 size_t nbrCount(size_t i, size_t j, const vector<vector<bool> >& g);
 void update();
 int initFromFile(const string& fname);
-/*FILE* f = fopen(fname, "rb");
-	int i = 0;
-	int j = 0;
-	char c;
-	while((c = getc(f))!= EOF) {
-		switch(c) {
-			case '.': grid[i][j] = false; ++i; break;
-			case 'O': grid[i][j] = true; ++i; break;
-			case '\n': ++j; i = 0; break;
-		}
-	}
-	for(int i = 0; i < 10; ++i){
-		for(int j = 0; j < 20; ++j) {
-			if(grid[j][i])
-				cout << "O";
-			else cout << ".";
-		}
-		cout << endl;
-	}
-	cout << endl;
-	fclose(f);
-	return 1;
-}
-*/
 void mainLoop();
 void dumpState(FILE* f);
 //extra functions 
@@ -186,7 +162,6 @@ int initFromFile(const string& fname){
 	if (!f) {
 	       	exit(1);
 	}
-
 	vector<bool> temp;
 	char c;
 	while ( fread(&c,1,1,f)!=0 ) {
